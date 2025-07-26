65f077d531360261f2ccadf5ceba758403f2eae4
Yuki <134689144+yuki10000@users.noreply.github.com>
2025-07-26 17:14:50 +0900

    #7 Lecturesアプリケーションのdjango-import-exportを定義した

--- backend/lectures/admin.py
+++ backend/lectures/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Lecture, LectureDetail, LectureDetailTime
 
-admin.site.register(Lecture)
-admin.site.register(LectureDetail)
-admin.site.register(LectureDetailTime)
+
+# Resource定義
+class LectureResource(resources.ModelResource):
+    class Meta:
+        model = Lecture
+
+
+class LectureDetailResource(resources.ModelResource):
+    class Meta:
+        model = LectureDetail
+
+
+class LectureDetailTimeResource(resources.ModelResource):
+    class Meta:
+        model = LectureDetailTime
+
+
+# Admin定義
+@admin.register(Lecture)
+class LectureAdmin(ImportExportModelAdmin):
+    resource_class = LectureResource
+
+
+@admin.register(LectureDetail)
+class LectureDetailAdmin(ImportExportModelAdmin):
+    resource_class = LectureDetailResource
+
+
+@admin.register(LectureDetailTime)
+class LectureDetailTimeAdmin(ImportExportModelAdmin):
+    resource_class = LectureDetailTimeResource
