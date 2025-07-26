0619fcead04bc904cf40cde1ea398f590efe210c
Yuki <134689144+yuki10000@users.noreply.github.com>
2025-07-26 17:25:58 +0900

    #7 Contactsアプリケーションのdjango-import-exportを定義

--- backend/contacts/admin.py
+++ backend/contacts/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Contact
 
-admin.site.register(Contact)
+
+# Resource定義
+class ContactResource(resources.ModelResource):
+    class Meta:
+        model = Contact
+
+
+# Admin定義
+@admin.register(Contact)
+class ContactAdmin(ImportExportModelAdmin):
+    resource_class = ContactResource
