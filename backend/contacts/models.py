66c7d20012c12b609abdbb316895f75d4287dd65
Yuki <134689144+yuki10000@users.noreply.github.com>
2025-07-30 21:10:04 +0900

    #10 Contactsアプリのモデルを変更

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -8,16 +8,14 @@ class Contact(models.Model):
     """
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    category = models.CharField(
-        max_length=50, blank=True, null=True
-    )
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    category = models.CharField(max_length=50, null=False, blank=True)
+    message = models.TextField(max_length=2000, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
     class Meta:
-        db_table = "contact"
+        db_table = "contacts"
 
     def __str__(self):
         return f"{self.name} <{self.email}>"
