a33b3bf22c417b3279f5d19464c98677399b39a5
atsukihat <atsukih46@gmail.com>
2025-08-13 17:21:00 +0900

    #17 openapiの検索バーの処理を定義

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,6 +155,13 @@ AUTH_USER_MODEL = 'users.User'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.'
+        'JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': (
         'rest_framework.pagination.PageNumberPagination'
     ),
